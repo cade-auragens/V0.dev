5e5c7061a6ef4e8de287a53f68ac69359cfdd0f6
cade-auragens <165055692+cade-auragens@users.noreply.github.com>
2025-01-23 14:20:32 -0600

    Create Logo.tsx

---
+++ Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center justify-end w-full">
+      <h1 className="text-2xl font-bold text-blue-600">AuraSports</h1>
+    </div>
+  )
+}
+
