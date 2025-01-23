39ff656d7093bd4ecbbb3bbcd18c3fa13c7e2bb7
cade-auragens <165055692+cade-auragens@users.noreply.github.com>
2025-01-23 16:20:37 -0600

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,14 @@
+import { inter, montserrat } from './lib/fonts'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
+
